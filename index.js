a329694666ac30a5019b1c7797126c60542256b3
Christopher Terrano <mterrano1@yahoo.com>
2021-11-03 13:18:26 -0500

    commit tree 386b10cb263976384e6b7a061453a37048b4b747

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+};
+
+const mondayWork = function(mondayActivity = 'go to the office') {
+    return `This Monday, I will ${mondayActivity}.`
+};
+
+function wrapAdjective(visualFlair = '*') {
+    return function(name = 'special') {
+        return `You are ${visualFlair}${name}${visualFlair}!`
+    }
+};
